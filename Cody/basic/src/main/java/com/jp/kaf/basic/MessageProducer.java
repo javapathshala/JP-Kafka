c2ca3eb44ab2a09d22f6566dfad79bd1f3746d30
Dimit Chadha <java.pathshala@gmail.com>
2019-01-11 16:55:56 +0530

    Added Basic Kafka Project Signed-off-by:Dimit Chadha <java.pathshala@gmail.com>

---
+++ Cody/basic/src/main/java/com/jp/kaf/basic/MessageProducer.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) Java Pathshala
+ * Wisdom Being Shared
+ * All rights reserved.
+ *
+ * No parts of this source code can be reproduced without written consent from
+ * Java Pathshala
+ * JavaPathshala.com
+ *
+ */
+package com.jp.kaf.basic;
+
+import java.io.IOException;
+import static java.lang.System.in;
+import java.util.Properties;
+import java.util.Scanner;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ *
+ *
+ *
+ * @author dimit.chadha
+ * @since 1.0
+ * @version 1.0
+ * @date Jan 11, 2019
+ */
+public class MessageProducer
+{
+
+    private Scanner scanner;
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception
+    {
+        MessageProducer messageProducer = new MessageProducer();
+        messageProducer.produceMessage(args[0]);
+    }
+
+    /**
+     *
+     * @param topicName
+     * @throws IOException
+     */
+    @SuppressWarnings("unchecked")
+    private void produceMessage(String topicName) throws IOException
+    {
+        scanner = new Scanner(System.in);
+        System.out.println("Enter message(type exit to quit)");
+
+        //Configure the Producer
+        Properties configProperties = new Properties();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer producer = new KafkaProducer(configProperties);
+
+        String line = scanner.nextLine();
+        while (!line.equals("exit"))
+        {
+            //TODO: Make sure to use the ProducerRecord constructor that does not take parition Id
+            ProducerRecord<String, String> rec = new ProducerRecord(topicName, line);
+            producer.send(rec);
+            line = scanner.nextLine();
+        }
+        in.close();
+        producer.close();
+
+    }
+}
